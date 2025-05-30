0af4c5b041115887153d0333f6bc19009339e1f5
Lina Bello <bellolina011@gmail.com>
2025-05-30 13:19:39 -0500

    [FEAT][CONFIG] Add Spring base 2

---
+++ kChingApp/src/main/java/co/com/kforge/kChingApp/KChingAppApplication.java
@@ -0,0 +1,13 @@
+package co.com.kforge.kChingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KChingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KChingAppApplication.class, args);
+	}
+
+}
---
+++ kChingApp/src/test/java/co/com/kforge/kChingApp/KChingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.kforge.kChingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KChingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
